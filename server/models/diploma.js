280bcde06c1a37aea7ddb2926756488de74b46d1
dhouha <dhouha.ouni@esprit.tn>
2017-04-07 00:22:36 +0100

    third

---
+++ server/models/diploma.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+
+//diplomas Schema
+
+var diplomaSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required:true
+    },
+    genre:{
+        type: String,
+        required:true
+    },
+    description:{
+        type: String
+    },
+
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+var Diploma = module.export = mongoose.model('Diploma', diplomaSchema);
+
+//GET diplomas
+module.exports.getDiplomas = function(callback, limit) {
+    Diploma.find(callback).limit(limit);
+};
+
+//Get diploma
+module.exports.getDiplomaById = function(id, callback) {
+    Diploma.findById(id, callback);
+
+};
+
+//ADD diploma
+module.exports.addDiploma = function(diploma, callback) {
+    Diploma.create(diploma, callback);
+};
+
+//Update diploma
+module.exports.updateDiploma = function (id, diploma, options, callback) {
+    var query = {_id: id};
+    var update = {
+        title:diploma.title,
+        genre:diploma.genre,
+        description:diploma.description
+
+    };
+    Diploma.findOneAndUpdate(query, update, options, callback);
+};
+
+
+//Delete diploma
+module.exports.removeDiploma = function(id,callback) {
+    var query = {_id: id};
+    Diploma.remove(query, callback);
+};
