1c0cd738a6cfd9245da75a2e91d83edb7ca5c9de
xoxo1234 <salma.chhoud@esprit.tn>
2017-04-14 01:58:59 +0100

    Commit

---
+++ models/university.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+
+var universitySchema = mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	category:{
+		type: String,
+        required: true
+
+	},
+        category1:{
+            type: String,
+            required: true
+        },
+	adresse:{
+		type: String,
+        minlength:1
+	},
+
+	image_url:{
+		type: String
+	}
+
+}
+    ,{collection : "university"}
+);
+
+var University = module.exports = mongoose.model('University', universitySchema);
