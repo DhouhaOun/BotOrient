c9dfedccb5db593206dbdf1209bb8f5e2716e7c0
xoxo1234 <salma.chhoud@esprit.tn>
2017-04-27 11:55:18 +0100

    hello world

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const uuid = require('node-uuid');
 const request = require('request');
 const JSONbig = require('json-bigint');
 const async = require('async');
+var fs = require('fs');
+var url = require('url');
+var progress = require('progress-stream');
 const REST_PORT = (process.env.PORT || 5000);
 const app = express();
 var db=require('./models/db');
@@ -97,6 +100,78 @@ app.post('/webhook/', (req, res) => {
 
 });
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now() + '-' + getExtension(file));
+    }
+});
+
+function getExtension(file) {
+    var res = '';
+    if (file.mimetype === 'image/jpeg') res = '.jpg';
+    if (file.mimetype === 'image/png') res = '.png';
+    return res;
+}
+
+app.use(express.static('./')); // serve all files in root folder, such as index.html
+
+var upload = multer({
+    storage: storage,
+    limits: { fileSize: 1048576, files: 1 }
+    //,fileFilter: // TODO limit types of files. currently can upload a .txt or any kind of file into uploads folder
+}).fields([
+    { name: "fileName", maxCount: 1 }
+]);
+app.post('/uploads', function (req, res, next) {
+    var prog = progress({time:100},function(progress){
+        var len = this.headers['content-length'];
+        var transf = progress.transferred;
+        var result = Math.round(transf/len * 100)+'%';
+    });
+
+    req.pipe(prog);
+    prog.headers = req.headers;
+
+    upload(prog, res, function (err) {
+        if (err) {
+            res.status(err.status || 500).json({ "error": { "status_code": err.status || 500, "message": err.code } });
+            return;
+        } else {
+
+            if (prog.files.fileName) {
+
+                res.writeHead(200,{'Content-Type':'text/html'});
+                var reqJSON = JSON.stringify(prog.files.fileName, null, 2); // pretty print the JSON for <pre> tag
+
+                res.write("<h1>Uploaded from file</h2><img style='max-width:20%' src='" + prog.files.fileName[0].path + "'/><a href='/#/upload'>Go back</a>");
+                res.end();
+            }
+            else if (prog.body.imageUrl) {
+                var download = function (uri, filename, callback) {
+                    request.head(uri, function (err, res, body) {
+                        console.log('content-type:', res.headers['content-type']);
+                        console.log('content-length:', res.headers['content-length']);
+                        request(uri).pipe(fs.createWriteStream('./uploads/' + filename)).on('close', callback);
+                    });
+                };
+                var urlParsed = url.parse(prog.body.imageUrl);
+                if (urlParsed.pathname){
+                    var onlyTheFilename = urlParsed.pathname ? urlParsed.pathname.substring(urlParsed.pathname.lastIndexOf('/') + 1).replace(/((\?|#).*)?$/, '') : '';
+                    var newFilename = onlyTheFilename + '-' + Date.now() + '-' + onlyTheFilename
+                    var wholePath = 'uploads/' + newFilename;
+                    download(urlParsed.href, newFilename, function () {
+                        var reqJSON = JSON.stringify(wholePath, null, 2); // pretty print
+                        res.end("<h1>Uploaded from URL</h2><img style='max-width:50%' src='" + wholePath + "'/><pre>" + reqJSON + "</pre><a href='/'>Go back</a>")
+
+                    });
+                }
+            }
+        }
+    });
+});
 app.listen(REST_PORT, () => {
     console.log('Rest service ready on port ' + REST_PORT);
 });
